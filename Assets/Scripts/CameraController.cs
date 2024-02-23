829922ef7807387e31d9e746078a0917abee5f6f
ABaccot <a.baccot@student.isartdigital.com>
2024-02-23 15:53:36 +0100

    Add suoerCamera (put -10 in Z for pointA and pointB)

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Vector3 pointA = default;
+    [SerializeField] private Vector3 pointB = default;
+    [SerializeField] private float slideDuration = default;
+    [SerializeField] private AnimationCurve slideCurve = default;
+    private bool isAtPointA = true;
+    private Coroutine currentCoroutine;
+
+    private void Awake()
+    {
+        TouchController.Instance.OnDown += Instance_OnDown;
+    }
+
+    private void Start()
+    {
+        SlideToOtherPoint();
+    }
+
+    private void Instance_OnDown(Vector2 point)
+    {
+        if (currentCoroutine != null) return;
+        currentCoroutine = StartCoroutine(SlideToOtherPoint());
+    }
+
+    public IEnumerator SlideToOtherPoint()
+    {
+        if (isAtPointA)
+        {
+            StartCoroutine(Tween.Move(transform.position, pointB, transform, slideDuration, slideCurve));
+            isAtPointA = false;
+        }
+        else
+        {
+            StartCoroutine(Tween.Move(transform.position, pointA, transform, slideDuration, slideCurve));
+            isAtPointA = true;
+        }
+
+        yield return new WaitForSeconds(slideDuration);
+        currentCoroutine = null;
+        yield return null;
+        
+    }
+}
